e6dafe917d3debaf7191a5e7f3212ee83b91e431
Charles Max Wood <chuck@devchat.tv>
2014-01-13 22:55:29 +0000

    cleanup

--- app/views/events/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @event, :id, :start_date, :end_date, :label_color_id, :created_at, :updated_at
--- spec/factories/events.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :event do
-    start_date "2014-01-09"
-    end_date "2014-01-09"
-    label_color nil
-  end
-end
